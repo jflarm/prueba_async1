a3bb2582b29a7a8e2cd4bd9d4df6f943740e65e4
Juan <jflarm@gmail.com>
2020-08-10 15:02:38 -0400

    Agregamos consulta para mas caros, luego hay que agregar columna para vendidos en la db y cambiar la consulta

--- app/controllers/controller_productos.php
+++ app/controllers/controller_productos.php
@@ -6,6 +6,7 @@
  	// echo 'Respuesta capturada: '.$peticion;
 
  	if($peticion == 'productos_nuevos'){
+
  		$sql = 'select * from productos where nuevo = 1';
  		$result = mysqli_query($connection, $sql);
  		$data=[];
@@ -17,5 +18,21 @@
 
  		sleep(4);
  		echo json_encode(array('data'=>$data));
+
+ 	}elseif($peticion == 'productos_destacados'){
+
+ 		$sql = 'select * from productos order by precio desc limit 2';
+ 		$result = mysqli_query($connection, $sql);
+ 		$data = [];
+
+ 		while($temp = mysqli_fetch_assoc($result)){
+ 			array_push($data, $temp);
+ 		}
+
+ 		echo json_encode(array('data'=>$data));
  	}
+ 	
+
+
+
  ?>
