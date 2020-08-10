b73b6bc766198973c56b039e4ec0c48f471c6f47
Juan <jflarm@gmail.com>
2020-08-10 17:33:49 -0400

    Ordenar por vendidos para obtener logica de productos_destacados.\nAun no esta la modificacion para mostrarlos en el template.

--- public/components/sub_items.php
+++ public/components/sub_items.php
@@ -5,21 +5,21 @@
         <!-- Start Left Sub Item -->
         <div class="sub_left">
           <div class="sub_items_header">
-            <h1>Laser Printer</h1>
-            <h2>Model 34-07-L</h2>
+            <h1 id="nombre_producto_1">Laser Printer</h1>
+            <h2 id="modelo_producto_1">Model 34-07-L</h2>
           </div>
-          <div class="sub_items_image"> <img src="<?php echo APP_ROOT ?>/public/images/item_printer.gif" width="167" height="164" alt="Sub Item Name" /> </div>
+          <div class="sub_items_image_2"> <img src="<?php echo APP_ROOT ?>/public/images/item_printer.gif" width="167" height="164" alt="Sub Item Name" /> </div>
           <div class="sub_items_text">
-            <p>If you're looking for beautiful and professionally made templates you can find them at Template Beauty.com.</p>
+            <p id="descripcion_producto_1">If you're looking for beautiful and professionally made templates you can find them at Template Beauty.com.</p>
             <p> <strong> Super performance <br />
               3 year warranty <br />
               1 year  free servicing </strong> </p>
           </div>
           <div class="sub_items_cartinfo">
             <div class="price">
-              <h2>$45.99</h2>
+              <h2 id="precio_producto_1">$45.99</h2>
             </div>
-            <div class="addtocart"> <a href="#"><span>Add to Cart</span></a> </div>
+            <div class="addtocart"> <a href="#"><span>Al Carrito</span></a> </div>
             <div class="clearthis">&nbsp;</div>
           </div>
           <div class="clearthis">&nbsp;</div>
@@ -28,21 +28,21 @@
         <!-- Start Right Sub Item -->
         <div class="sub_right">
           <div class="sub_items_header">
-            <h1>Scanner</h1>
-            <h2>Model 66-0l-X</h2>
+            <h1 id="nombre_producto_2">Scanner</h1>
+            <h2 id="modelo_producto_2">Model 66-0l-X</h2>
           </div>
-          <div class="sub_items_image"> <img src="<?php echo APP_ROOT ?>/public/images/item_scanner.gif" width="175" height="170" alt="Sub Item Name" /> </div>
+          <div class="sub_items_image_2"> <img src="<?php echo APP_ROOT ?>/public/images/item_scanner.gif" width="175" height="170" alt="Sub Item Name" /> </div>
           <div class="sub_items_text">
-            <p>To find great hosting providers visit Web Hosting Zoom</p>
+            <p id="descripcion_producto_1">To find great hosting providers visit Web Hosting Zoom</p>
             <p> <strong> Super performance <br />
               3 year warranty <br />
               1 year  free servicing </strong> </p>
           </div>
           <div class="sub_items_cartinfo">
             <div class="price">
-              <h2>$45.99</h2>
+              <h2 id="precio_producto_2">$45.99</h2>
             </div>
-            <div class="addtocart"> <a href="#"><span>Add to Cart</span></a> </div>
+            <div class="addtocart"> <a href="#"><span>Al Carrito</span></a> </div>
             <div class="clearthis">&nbsp;</div>
           </div>
           <div class="clearthis">&nbsp;</div>
--- public/js/productos.js
+++ public/js/productos.js
@@ -24,7 +24,6 @@ function productosNuevos(){
 		},
 		success: function(response){
 			
-			
 			let result = JSON.parse(response)
 			let i = 0
 
@@ -72,5 +71,74 @@ function productosNuevos(){
 			$('#new_item_link').hide()
 		}
 	})
+
+
+// Funcion para mostrar productos nuevos
+
+// function productosDestacados(){
+// 	$.ajax({
+// 		url: 'app/controllers/controller_productos.php',
+// 		type: 'POST',
+// 		data: {
+// 			peticion: 'productos_destacados'
+// 		},
+// 		success: function(response){
+			
+// 			let result = JSON.parse(response)
+// 			let i = 0
+
+// 			mostrar_producto({
+// 				nombre_producto: result.data[i].nombre_producto,
+// 				modelo_producto: result.data[i].modelo,
+// 				imagen_producto: result.data[i].imagen_producto,
+// 				precio_producto: result.data[i].precio_producto,
+// 				descripcion_producto: result.data[i].descripcion
+// 			})
+// 			$('#new_item_link').show()
+
+// 			setInterval(function(){
+
+// 				mostrar_producto({
+// 					$('#nombre_producto_1').html(arg.nombre_producto)
+// 					$('#modelo_producto_1').html(arg.modelo_producto)
+// 					$('#imagen_producto_1').attr('src',arg.imagen_producto)
+// 					$('#descripcion_producto_1').html(arg.descripcion_producto)
+
+// 					$('#nombre_producto_2').html(arg.nombre_producto)
+// 					$('#modelo_producto_2').html(arg.modelo_producto)
+// 					$('#imagen_producto_2').attr('src',arg.imagen_producto)
+// 					$('#descripcion_producto_2').html(arg.descripcion_producto)
+// 				})				
+
+// 				if(i == result.data.length - 1){
+// 					i = 0
+// 				}else{
+// 					i++
+// 				}
+// 			}, 5000)
+
+// 		},
+
+
+// 		error: function( jqXHR, textStatus, errorThrown){
+// 			console.log(textStatus+' '+errorThrown)
+// 		},
+// 		complete: function(){
+// 			console.log('Completado.')
+
+// 		},
+// 		beforeSend: function(){
+
+// 			mostrar_producto({
+// 					nombre_producto: 'Cargando...',
+// 					modelo_producto: 'Nuestros nuevos productos...',
+// 					imagen_producto: 'https://i.pinimg.com/originals/1c/49/2e/1c492eb6e1d085b2fa32302ecd6f8343.gif',
+// 					descripcion_producto: 'Estos son nuestros nuevos productos'
+// 				})
+// 			$('#new_item_link').hide()
+// 		}
+// 	})
+
+
 
 }
