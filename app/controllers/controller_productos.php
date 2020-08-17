c512c9477b5d8fb32707d5721fc98c8f111f2476
Juan <jflarm@gmail.com>
2020-08-17 12:02:26 -0400

    Renombrar variables y solucion de errores en controller_productos.php

--- app/controllers/controller_productos.php
+++ app/controllers/controller_productos.php
@@ -1,5 +1,9 @@
 <?php 
  
+    //  3 lineas muestran errores de PHP en el browser
+ //    ini_set('display_errors', 1);
+ //    ini_set('display_startup_errors', 1);
+	// error_reporting(E_ALL);
 
 	require_once '../core/config.php';
  	$peticion = $_POST['peticion'];
@@ -10,36 +14,30 @@
 
  		// Llamamos a la funcion que ejecuta el query y le pasamos el query para
  		// productos nuevos
- 		$new_products = executeQuery('select * from productos where nuevo = 1');
+ 		$new_products = executeQuery('select * from productos where nuevo = 1', $connection);
 
  		// Se devuelve un json con los datos resultantes de la consulta
  		echo json_encode(array('data'=>$new_products, 'peticion'=>'productos_nuevos'));
- 		}
+ 		
+  	}elseif($peticion == 'productos_destacados'){
 
- 		sleep(2);
- 		echo json_encode(array('data'=>$data, 'peticion'=>'productos_nuevos'));
+ 		$top_products =  executeQuery('select * from productos where nuevo != 1 order by vendidos desc limit 6', $connection);
+ 		
+ 		echo json_encode(array('data'=>$top_products, 'peticion'=>'productos_destacados'));
+ 		
 
- 	}elseif($peticion == 'productos_destacados'){
-
- 		$sql = 'select * from productos where nuevo != 1 order by vendidos desc limit 6';
- 		$result = mysqli_query($connection, $sql);
- 		$data = [];
-
- 		while($temp = mysqli_fetch_assoc($result)){
- 			array_push($data, $temp);
- 		}
-
- 		sleep(2);
- 		echo json_encode(array('data'=>$data, 'peticion'=>'productos_destacados'));
  	}elseif ($peticion == 'buscar') {
- 		echo json_encode(array('data'=>$_POST['valor'], 'peticion'=>'buscar'));
+
+ 		$valor = $_POST['valor'];
+
+ 		$buscar_productos = executeQuery('select * from productos where nombre_producto like "%'.$valor.'%" ', $connection);
+ 		echo json_encode(array('data'=>$buscar_productos, 'peticion'=>'buscar'));
  	}
  	
  	// Functions
 
- 	function executeQuery($query){
- 		$sql = '$query;
- 		$result = mysqli_query($connection, $sql);
+ 	function executeQuery($query, $connection){
+ 		$result = mysqli_query($connection, $query);
  		$data=[];
 
  		// print_r ($connection);
